616d615f4dfcd4101ae8c11583fb9329a0b05349
Iruam Parreira <iruamfx@gmail.com>
2025-02-21 11:48:33 -0300

    @iruamfx

--- Proj-Game-Web/app.py
+++ Proj-Game-Web/app.py
@@ -1,19 +1,26 @@
 # Importa pacote Flask do modulo flask
-from flask import Flask #as app
+from flask import Flask, render_template #as app
 
 # Instanciando Flask no app.py
-app = Flask(__name__)
+app = Flask(__name__, template_folder="views")
 
 # Rota principal do site
 @app.route("/")
-
-# Criar função da home
+# View function - Explicita o que vai ser renderizado na rota
 def home():
-    return "<h1>Hello Flask!</h1>"
+    return render_template("index.html")
 
 @app.route("/games")
 def games():
-    return "<h1>Pagina de games</h1>"
+    # class Game:
+    #     def __init__:
+    #         self.
+    # year = 2012
+    # category = "Online FPS"
+    games_title = ["Red Dead Redemption 2", "Grand Theft Auto V", "DCS World", "Helldivers 2", "Forza Horizon 4", "God of War", "Assassin's Creed Origins"]
+    
+    players = ["iruah", "davi_lambari", "edsongf"]
+    return render_template("games.html", title=title, year=year, category=category, players=players)
 
 if __name__ == "__main__":
     # Roda servidor no localhost, porta 5000; 0.0.0.0 = rede LAN
